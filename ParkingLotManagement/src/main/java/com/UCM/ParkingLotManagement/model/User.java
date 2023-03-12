16f3a4fd50d892528dbd7e385f3a72c835d8a048
sahithmanchi <sahithmanchi@sahiths-mbp.attlocal.net>
2023-03-11 20:24:05 -0600

    this is a initial commit for the project

---
+++ ParkingLotManagement/src/main/java/com/UCM/ParkingLotManagement/model/User.java
@@ -0,0 +1,7 @@
+package com.UCM.ParkingLotManagement.model;
+
+public class User {
+	
+	
+
+}
---
+++ ParkingLotManagement/src/test/java/com/UCM/ParkingLotManagement/ParkingLotManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UCM.ParkingLotManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
